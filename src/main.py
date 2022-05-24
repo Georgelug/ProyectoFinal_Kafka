62a1fb6e7c416346719fafd6ccb522698399c0b5
Octavio Gonzalez <octavio_ga@hotmail.com>
2022-05-24 00:32:59 -0500

    (develop) Begin

--- src/main.py
+++ src/main.py
@@ -2,16 +2,35 @@
 from flask import Flask, render_template, request
 from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
+from wtforms.fields import SubmitField, TextAreaField, StringField
 
-from wtforms.fields import SubmitField
+from ProducerConsumer import Broker
 
 app = Flask(__name__, static_folder='static')
 bootstrap = Bootstrap(app)
 app.config['SECRET_KEY'] = 'awa de uwu'
+ 
+brokers = [ Broker(active_status=True, broker_id=1), Broker(active_status=True, broker_id=2)]
+
+class ProducerForm(FlaskForm):
+    topic = StringField(name = "Topic")
+    message = StringField(name ="Mensaje")
+    submitButton = SubmitField("Enviar mensaje")
+
 
 @app.route('/')
 def home():
     return render_template('index.html')
 
+@app.route('/Producer1')
+def producer1():
+    message = ProducerForm()
+    print( message.message)
+    context ={
+        'messageForm': message
+    }
+    return render_template('producer.html', **context)
+
 if __name__ == '__main__':
     app.run()
+    
