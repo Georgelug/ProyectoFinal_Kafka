9fe52e5297dc93ea7c3141af2ae6cfd5abb42ef7
Octavio Gonzalez <octavio_ga@hotmail.com>
2022-05-23 23:47:04 -0500

    (develop) agregada la base del proyecto

--- src/main.py
+++ src/main.py
@@ -1,1 +1,17 @@
 # server donde van a estar los brokers
+from flask import Flask, render_template, request
+from flask_bootstrap import Bootstrap
+from flask_wtf import FlaskForm
+
+from wtforms.fields import SubmitField
+
+app = Flask(__name__, static_folder='static')
+bootstrap = Bootstrap(app)
+app.config['SECRET_KEY'] = 'awa de uwu'
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run()
